ad309847b0c1abab4a845db26f97e61e41d53652
João Ferreira <joaovtf39@gmail.com>
2022-04-03 01:22:12 -0300

    conecta com o db na supabase

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const pokemonsRoute_1 = __importDefault(require("./routes/pokemonsRoute"));
 const userRegisterRoute_1 = __importDefault(require("./routes/userRegisterRoute"));
 const loginRoute_1 = __importDefault(require("./routes/loginRoute"));
 const handleError_1 = __importDefault(require("./middlewares/handleError"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use('/pokemons', pokemonsRoute_1.default);
 app.use('/register', userRegisterRoute_1.default);
